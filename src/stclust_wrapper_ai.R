01fdadb54f67f55a2b81c002e7f8722632dd1f5e
Aditya Kakarla <85147239+adityakakarla@users.noreply.github.com>
2024-10-02 11:34:25 -0700

    updated module code to allow comma-separated inputs custom filepaths

--- src/stclust_wrapper_ai.R
+++ src/stclust_wrapper_ai.R
@@ -15,6 +15,10 @@ option_list <- list(
               help = "Comma-separated list of metadata file paths", metavar = "character"),
   make_option(c("-a", "--samples"), type = "character", default = NULL, 
               help = "Comma-separated list of sample names", metavar = "character"),
+  make_option(c("-f", "--keep_fovs"), type="character", default=NULL,
+              help = "Comma-separated list of FOVs to keep", metavar = "character"),
+  make_option(c("-i", "--image_paths"), type="character", default=NULL,
+              help = "Comma-separated list of image paths", metavar = "character"),
 
   make_option(c("-w", "--ws"), type="double", default=0.025, 
               help="Weight to be applied to spatial distances (0-1). Default is 0.025", metavar="double"),
@@ -31,67 +35,36 @@ option_list <- list(
   make_option(c("-p", "--plot"), type="logical", default=FALSE, 
               help="Option to plot intermediate results. Default is FALSE", metavar="logical"),
   make_option(c("-o", "--output"), type="character", default="output.RData", 
-              help="Path to save the output STlist object. Default is 'output.RData'", metavar="character"),
-  make_option(c("-f", "--keep_fovs"), type = "character", default = NULL, 
-              help = "Comma-separated list of FOVs to keep. If not specified, no filtering is done.", metavar = "character"),
-  
-  # Additional filter_data parameters
-  make_option(c("--spot_minreads"), type = "integer", default = 20, 
-              help = "Minimum number of reads per spot. Default is 20", metavar = "integer"),
-  make_option(c("--rm_genes_expr"), type = "character", default = "^NegPrb", 
-              help = "Regular expression to filter out genes. Default is '^NegPrb'", metavar = "character"),
-  make_option(c("--rm_tissue"), type = "character", default = NULL, 
-              help = "Comma-separated list of tissues to remove. Default is NULL (no filtering)", metavar = "character"),
-
-  # Option for image archives
-  make_option(c("-i", "--images"), type="character", default=NULL, 
-              help="Path to a text file with tar archive file paths, one per row, for each sample.", metavar="character")
+              help="Path to save the output STlist object. Default is 'output.RData'", metavar="character")
 )
 
 # Parse command line options
 opt <- parse_args(OptionParser(option_list=option_list))
 
-exprmats <- readLines(opt$exprmats)
-metas <- readLines(opt$metas)
+# Split comma-separated file paths
+exprmats <- unlist(strsplit(opt$exprmats, ","))
+metas <- unlist(strsplit(opt$metas, ","))
 samples <- unlist(strsplit(opt$samples, ","))
-image_archives <- readLines(opt$images)
+keep_fovs <- unlist(strsplit(opt$keep_fovs, ","))
+image_paths <- unlist(strsplit(opt$image_paths, ","))
+
+print(exprmats)
 
 # Load data
 lung <- STlist(rnacounts = exprmats, spotcoords = metas, samples = samples)
-lung <- filter_data(lung, spot_minreads=opt$spot_minreads, rm_genes_expr=opt$rm_genes_expr)
+lung <- filter_data(lung, spot_minreads=20, rm_genes_expr='^NegPrb')
 summ_df = summarize_STlist(lung)
 lung <- transform_data(lung, method='sct')
 
-# Filter FOVs if specified
-if (!is.null(opt$keep_fovs)) {
-  keep_fovs <- unlist(strsplit(opt$keep_fovs, ","))
-  rm_fovs = summ_df$sample_name[!(summ_df$sample_name %in% keep_fovs)] 
-  lung_subset <- filter_data(lung, rm_tissue=rm_fovs)
-} else {
-  lung_subset <- lung
-}
-
-# Extract images from tar archives and load them
-for (tar_file in image_archives) {
-  # Create a temporary directory for extraction
-  temp_dir <- tempfile(pattern = "image_extract_")
-  dir.create(temp_dir)
-  
-  # Extract the tar archive
-  untar(tar_file, exdir = temp_dir)
-  
-  # Get all image file paths from the extracted directory
-  image_files <- list.files(temp_dir, recursive = TRUE, full.names = TRUE)
-  
-  # Load images into the lung_subset
-  lung_subset <- load_images(lung_subset, images=image_files)
-  
-  # Clean up temporary directory after use
-  unlink(temp_dir, recursive = TRUE)
-}
+keep_fovs
+rm_fovs = summ_df$sample_name[!(summ_df$sample_name %in% keep_fovs)] 
+lung_subset <- filter_data(lung, rm_tissue=rm_fovs)
 
 # Perform clustering using STclust
-lung_subset <- STclust(x=lung_subset, ws=opt$ws, ks=opt$ks)
+lung_subset <- STclust(x=lung_subset, ws=opt$ws, ks = opt$ks)
+for(image_path in image_paths){
+  lung_subset <- load_images(lung_subset, images=image_path)
+}
 ti <- plot_image(lung_subset)
 dom_p <- STplot(lung_subset, ks='dtc', ws=0.02, deepSplit=F, color_pal='discreterainbow')
 
@@ -108,5 +81,9 @@ save(lung_subset, file=opt$output)
 # Print completion message
 cat("Clustering completed and results saved to", opt$output, "\n")
 
-traceback()
-
+# Rscript --vanilla /genepattern/stclust_wrapper_ai.R \
+# --exprmats /data/Lung5_Rep1/Lung5_Rep1-Flat_files_and_images/Lung5_Rep1_exprMat_file.csv,/data/Lung6/Lung6-Flat_files_and_images/Lung6_exprMat_file.csv \
+# --metas /data/Lung5_Rep1/Lung5_Rep1-Flat_files_and_images/Lung5_Rep1_metadata_file.csv,/data/Lung6/Lung6-Flat_files_and_images/Lung6_metadata_file.csv \
+# --samples Lung5_Rep1,Lung6 \
+# --keep_fovs Lung5_Rep1_fov_6,Lung6_fov_6 \
+# --image_paths /data/Lung5_Rep1/Lung5_Rep1-Flat_files_and_images/CellComposite,/data/Lung6/Lung6-Flat_files_and_images/CellComposite
